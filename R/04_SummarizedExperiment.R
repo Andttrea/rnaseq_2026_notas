e6ac746245b0222293e496b5221bc8d7407b6942
Andrea Villarruel Garcia <andreavg@lcg.unam.mx>
2026-02-11 12:54:31 -0600

    Remove extra blank line and comment out spatialLIBD data download code

--- R/04_SummarizedExperiment.R
+++ R/04_SummarizedExperiment.R
@@ -63,7 +63,6 @@ rse[1:2, ]
 ## Comando 2
 rse[, c("A", "D", "F")]
 
-
 ## ----isee_basic, eval = FALSE---------------------------------------------------
 # ## Explora el objeto rse de forma interactiva
 # library("iSEE")
@@ -71,11 +70,11 @@ rse[, c("A", "D", "F")]
 
 ## ----download_sce_layer---------------------------------------------------------
 ## Descarguemos unos datos de spatialLIBD
-sce_layer <- spatialLIBD::fetch_data("sce_layer")
-sce_layer
+#sce_layer <- spatialLIBD::fetch_data("sce_layer")
+#sce_layer
 
 ## Revisemos el tamaño de este objeto
-lobstr::obj_size(sce_layer)
+#lobstr::obj_size(sce_layer)
 
 ## ----explore_sce_layer, eval = FALSE--------------------------------------------
 # iSEE::iSEE(sce_layer)
